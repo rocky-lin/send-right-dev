71720d502366c036e6e47322e8c1ef054d1be13f
michael andrew suarez <suarezandrew441@gmail.com>
2017-03-27 15:58:57 +0800

    add function curl to call api payshortcut

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -5,28 +5,145 @@ if(!function_exists('test')) {
 		print "this is the test helper functionality";
 	}  
 }
-function print_r_pre($string, $title=null)
-{
 
-	if(!empty($title)){
-		print "<br> " . $title . '<br>';
+if(!function_exists('print_r_pre')) {
+	function print_r_pre($string, $title = null)
+	{
+
+		if (!empty($title)) {
+			print "<br> " . $title . '<br>';
+		}
+
+		print "<pre>";
+		print_r($string);
+		print "</pre>";
 	}
+}
 
-	print "<pre>";
+if(!function_exists('print_r_pre_die')) {
+
+	function print_r_pre_die($string)
+	{
+		print "<pre>";
 		print_r($string);
-	print "</pre>";
+		print "</pre>";
+		exit;
+	}
 }
-function print_r_pre_die($string)
+if(!function_exists('isValidEmail'))
 {
-	print "<pre>";
-		print_r($string);
-	print "</pre>";
-	exit;
+	function isValidEmail($email)
+	{
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
-function isValidEmail($email) {  
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-     	return false;
-    } else {
-    	return true;
-    }
+
+if(!function_exists('changeDashToSpaceUcLetter')) {
+	function changeDashToSpaceUcLetter($string)
+	{
+		$string = str_replace('_', ' ', $string);
+		return ucfirst($string);
+	}
+}
+
+/**
+ * @src http://stackoverflow.com/questions/2138527/php-curl-http-post-sample-code
+ * @param $postData
+ * @return mixed
+ */
+if(!function_exists('curlPostRequest')) {
+	function curlPostRequest($postData, $url)
+	{
+		$ch = curl_init($url);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+		// execute!
+		$response = curl_exec($ch);
+
+		// close the connection, release resources used
+		curl_close($ch);
+
+		// do anything you want with your response
+		var_dump($response);
+
+		return json_decode($response, true);
+	}
+}
+
+/**
+ * @src http://codular.com/curl-with-php
+ * @param $getData
+ * @param $url
+ * @return mixed
+ */
+if(!function_exists('curlGetRequest')) {
+	function curlGetRequest($getData, $url)
+	{
+
+		$getData = http_build_query($getData, "", "&");
+
+		$url = $url . '?' . $getData;
+
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+
+		return json_decode($output, true);
+	}
+}
+
+if(!function_exists('addpadding')) {
+
+	function addpadding($string, $blocksize = 32)
+	{
+		$len = strlen($string);
+		$pad = $blocksize - ($len % $blocksize);
+		$string .= str_repeat(chr($pad), $pad);
+		return $string;
+	}
+}
+if(!function_exists('time_elapsed_string')) {
+
+	function time_elapsed_string($datetime, $full = false)
+	{
+		$now = new DateTime;
+		$ago = new DateTime($datetime);
+		$diff = $now->diff($ago);
+
+		$diff->w = floor($diff->d / 7);
+		$diff->d -= $diff->w * 7;
+
+		$string = array(
+				'y' => 'year',
+				'm' => 'month',
+				'w' => 'week',
+				'd' => 'day',
+				'h' => 'hour',
+				'i' => 'minute',
+				's' => 'second',
+		);
+		foreach ($string as $k => &$v) {
+			if ($diff->$k) {
+				$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+			} else {
+				unset($string[$k]);
+			}
+		}
+
+		if (!$full) $string = array_slice($string, 0, 1);
+		return $string ? implode(', ', $string) . ' ago' : 'just now';
+	}
 }
